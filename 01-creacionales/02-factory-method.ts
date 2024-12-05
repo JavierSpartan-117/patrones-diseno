30ff8b376f913e46a1c322ddf411ea92b3520f7d
JavierSpartan-117 <ncalderont2@miumg.edu.gt>
2024-12-05 16:25:40 -0600

    Patron Factory Method

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,84 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburguer {
+    prepare(): void;
+}
+
+class ChickenHamburger implements Hamburguer {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cpollo', COLORS.yellow );
+    }
+
+}
+
+class BeefHamburger implements Hamburguer {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %ccarne', COLORS.brown);
+    }
+
+}
+
+class BeanHamburger implements Hamburguer {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cfrijol', COLORS.green);
+    }
+
+}
+
+abstract class Restaurant {
+    abstract createHamburger(): Hamburguer;
+
+    orderHamburger():void {
+        const hamburger = this.createHamburger();
+        hamburger.prepare();
+    }
+}
+
+class ChickenRestaurant extends Restaurant {
+    createHamburger(): Hamburguer {
+        return new ChickenHamburger()
+    }
+}
+
+class BeefRestaurant extends Restaurant {
+    createHamburger(): Hamburguer {
+        return new BeefHamburger()
+    }
+}
+
+class BeanRestaurant extends Restaurant {
+    createHamburger(): Hamburguer {
+        return new BeanHamburger()
+    }
+}
+
+function main() {
+    let restaurant: Restaurant;
+
+    const burgerType = prompt('¿Que tipo de hamburguesa quieres? (chicken/beef/bean)')
+
+    switch(burgerType) {
+        case 'chicken':
+            restaurant = new ChickenRestaurant();
+            break;
+
+        case 'beef':
+            restaurant = new BeefRestaurant();
+            break;
+
+        case 'bean':
+            restaurant = new BeanRestaurant();
+            break;
+
+        default:
+            throw new console.error('Opcion no valida');
+    }
+
+    restaurant.orderHamburger();
+}
+
+main();
